4cda35cd7e77b34ce606f7b4fe55f6ca4ab12f50
DylanJoey17 <dharve3@clemson.edu>
2023-09-05 10:05:41 -0400

    Added .gitignore, fixed lab1.c

---
+++ lab/lab1/lab1.c
@@ -0,0 +1,32 @@
+/*	Dylan Harvey
+	CpSc 1010 Lab, Fall 2023
+	Lab #1
+	My first C program
+*/
+
+#include <stdio.h>
+
+int main(void)	{
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	printf("Go Tigers!!\n");
+	return 0;
+}
+
