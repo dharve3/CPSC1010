d792f955e64b03f2f2c0faf347e23082cd7d2499
DylanJoey17 <dharve3@clemson.edu>
2023-09-20 09:49:06 -0400

    Added max_min_score.c

---
+++ lecture/practice/max_min_score.c
@@ -0,0 +1,36 @@
+/* max_min_score.c */
+
+#include <stdio.h>
+
+int main(void) {
+   // 1. declare the 3 needed variables here: num, maxScore, minScore
+	int num, maxScore, minScore;
+
+   printf("\nEnter numbers between 0 and 100, inclusive:  \n");
+   printf("Hit ctrl-d when done.\n\n");
+   // 2. write the scanf needed to get the first value entered by the user
+	scanf("%d", num)
+
+   // 3. initialize other two variables here
+
+
+
+   // 4. complete the condition in the while loop
+   while (         ) {    
+      // 5. body of the while loop to keep track of the maximum score 
+		//    and the minimum score
+
+
+
+
+		// 6. another scanf to get the next value
+		// hint: endOfData.c may give a hint on how to do this
+		
+   }
+
+   printf("\nMaximum score is %d\n", maxScore);
+   printf("Minimum score is %d\n\n", minScore);
+
+	return 0;
+}
+
