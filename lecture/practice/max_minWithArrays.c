1f75d3e78eed5d69a37aa7867471b5cc1cc6a099
DylanJoey17 <dharve3@clemson.edu>
2023-09-22 09:44:13 -0400

    Completed max_minWithArrays.c, added input.txt for ease of testing

--- lecture/practice/max_minWithArrays.c
+++ lecture/practice/max_minWithArrays.c
@@ -4,37 +4,31 @@ int main(void) {
    // 1. declare the 4 needed variables here: 
 	//		array called num that can hold 10 integers, 
 	// 	integers maxScore, minScore, i
-
-
+	int num[10] = {0};
+	int maxScore, minScore, i;
 
    printf("\nEnter numbers between 0 and 100, inclusive:  \n");
    // 2. write the loop to get the values from the user 
-	
-
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &num[i]);
+	}
 
 
    // 3. initialize other two variables here
+	maxScore = 0;
+	minScore = 100;
 
-
-				
 	i = 0;
    // 4. complete the condition in the while loop
-   while (            ) {
+   while ( i < 10 ) {
       // 5. body of the while loop to keep track of the maximum score 
 		//    and the minimum score
- 
+ 		if (num[i] > maxScore)
+			maxScore = num[i];
  		
+		if (num[i] < minScore)
+			minScore = num[i];
 	
-
-
-
-
-
-
-
-
-
-
 		i++;	
    }
 
