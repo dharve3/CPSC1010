296b97b1c0fa577f67d9dbabb93dd0c1d5c039a1
DylanJoey17 <dharve3@clemson.edu>
2023-10-03 09:56:11 -0400

    updated progtemp.c

--- lecture/practice/progtemp.c
+++ lecture/practice/progtemp.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
 int main() {
-   printf("%d", (int)sizeof(int));
+	int i;
+	int grade, sum, howMany, count;
+	float average;
+
+	printf("How many grades will you be entering? \n");
+	scanf("%i", &howMany);
+
+	// blah blah
+	count = howMany;
+	sum = 0;
+	
+	/*
+	while (count > 0) {
+		printf("Enter a grade: \n");
+		scanf("%i", &grade);
+		sum += grade;
+		count = count - 1;
+	}
+	*/
+	
+	/*
+	for (i = count; i > 0; i--) {
+		printf("Enter a grade: \n");
+		scanf("%i", &grade);
+		sum += grade;
+	}
+	*/
+
+	do {
+		printf("Enter a grade: \n");
+		scanf("%i", &grade);
+		sum += grade;
+		count -= 1;
+	}
+	while(count > 0);
+
+	average = (float)sum / howMany;
+	printf("The average is: %f\n\n", average);
+
+	return 0;
 }
