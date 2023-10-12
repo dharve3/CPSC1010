4d44e5fbf91fd6307e1f813bb8edf9949c07b276
DylanJoey17 <dharve3@clemson.edu>
2023-10-12 10:23:46 -0400

    Working lab6.c with debug statements

--- lab/lab6/lab6.c
+++ lab/lab6/lab6.c
@@ -13,21 +13,21 @@ const int MAX_LINE = 20;
 
 int main(void) {
 	// read input from file
-	int i, j, temp;
-	int line_len;
+	int i, j, gender, weight_lbs, height_in, age, temp;
+	float weight_kg, height_cm;
+	double BMR, calories;
 	char line_in[MAX_LINE];
 	char activity_type[NUM_ACTIVITIES][MAX_LINE];
 	float activity_level[NUM_ACTIVITIES];
 
 	for (i = 0; i/2 < NUM_ACTIVITIES; i++) {
 		printf("-----------------------------------\n");
-		printf("DEBUG: i = %d, i/2 = %d, i%2 = %d\n", i, i/2, i%2);
+		printf("DEBUG: i = %d, i/2 = %d, imod2 = %d\n", i, i/2, i%2);
 		if (i % 2 == 0) {
 			fgets(line_in, MAX_LINE, stdin);
-			line_len = strlen(line_in);
 			printf("DEBUG: Line In: %s", line_in);
 			printf("DEBUG: Assigning 2D\n");
-			for (j = 0; j < line_len; j++) {
+			for (j = 0; j < MAX_LINE; j++) {
 				activity_type[i/2][j] = line_in[j];
 				printf("Char: '%c' ", line_in[j]);
 			}
@@ -40,5 +40,44 @@ int main(void) {
 			printf("DEBUG: Activity Level: %f\n", activity_level[i/2]);
 		}
 	}
+	// Ask user for values
+	// COMMENT THIS LINE OUT BEFORE GRADESCOPE SUBMISSION:
+	freopen("/dev/tty", "rw", stdin);
+
+	while (gender != 1 && gender != 2) {
+		printf("\n\nAre you a boy or a girl? 1 for boy, 2 for girl: ");
+		scanf("%d", &gender);
+	}
+	printf("\nYour weight (pounds): ");
+	scanf("%d", &weight_lbs);
+	printf("Your height (inches): ");
+	scanf("%d", &height_in);
+	printf("Your age (years): ");
+	scanf("%d", &age);
 	
+	// calculate values
+	weight_kg = (float)weight_lbs / 2.205;
+	height_cm = height_in * 2.54;
+
+	if (gender == 1) {
+		printf("DEBUG: Calculating for boy");
+		BMR = (10 * weight_kg + 6.25 * height_cm - 5 * age + 5);
+	}
+	else if (gender == 2) {	
+		printf("DEBUG: Calculating for girl");
+		BMR = (10 * weight_kg + 6.25 * height_cm - 5 * age - 161);
+	}
+	// print results
+	printf("\n\nYour BMR (basal metabolic rate) is %.1lf", BMR);
+	printf("\n\nBMR represents the amount of calories you burn at rest."
+			 "\n\nTotal daily calories needed to maintain your current "
+			 "weight at a\nparticular activity levels is "
+			 "your BMR * that activity level value:\n\n");
+	
+	// calculate and print daily calorie intake results
+	for (i = 0; i < NUM_ACTIVITIES; i++) {
+		calories = BMR * activity_level[i];
+		printf("%.1lf calories per day for %s", calories, activity_type[i]);
+	}
+
 }
