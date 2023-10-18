d9a5fe005c952cc97740052b5d9b12626d9105c0
DylanJoey17 <dharve3@clemson.edu>
2023-10-17 21:46:58 -0400

    Updated minValueWithArrays.c

--- lecture/practice/minValueWithArrays.c
+++ lecture/practice/minValueWithArrays.c
@@ -8,9 +8,13 @@ int main() {
 	int array2[] = { 12, 45, 1, 10, 5, 3, 22 };
 	int min; 
 		
+	// NOTE: You can determine size externally per array here
+	int size1 = sizeof(array1) / sizeof(array1[0]);
+	printf("DEBUG: size1 == %d", size1);
+	// OR: pass it thorugh manually (any int works but match size to array duh)
 
 	// function call 1
-	min = getMin(array1, 5);
+	min = getMin(array1, size1);
 	/*min = array1[0];
 	for (int i = 1; i < 5; i++) {
 		if (min > array1[i])
@@ -19,8 +23,11 @@ int main() {
 
 	printf("\nminimum value: %d\n\n", min);
 
+	// NOTE: Size using same method for array2
+	int size2 = sizeof(array2) / sizeof(array2[0]);
+	printf("DEBUG: size2 == %d", size2);
 	// function call 2
-	min = getMin(array2, 7);
+	min = getMin(array2, size2);
 	/* min = array2[0];
 	for (int i = 1; i < 7; i++) {
 		if (min > array2[i])
@@ -39,9 +46,18 @@ int getMin(int theArray[], int size) {
 	// local vars
 	int minimum;
 	// int size;
-
+	
+	/* NOTE: I'm under the impression it is impossible to determine size of
+	the array using this method as arrays decay to pointers when passed 
+	through to a function as an argument.*/
+	// int temp1 = sizeof(theArray);
+	// int temp2 = sizeof(theArray[0]);
+	// int temp3 = sizeof(arr);
 	// size = sizeof(theArray) / sizeof(theArray[0]);
-	// This doesnt work since sizeof(theArray) returns the sizeof(int *)
+	// printf("DEBUG: sizeof(theArray) == %d, sizeof(theArray[0]) == %d\n", temp1, temp2);
+	// printf("DEBUG: size == %d\n", size);
+	// printf("DEBUG: pointer size == %d", temp3);
+
 	minimum = theArray[0];
 	for (int i = 1; i < size; i++) {
 		if (minimum > theArray[i])
