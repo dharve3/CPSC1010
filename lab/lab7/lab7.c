39b575661956b9711fd60359c9dbe66e84f9408c
DylanJoey17 <dharve3@clemson.edu>
2023-10-24 10:19:44 -0400

    Completed lab7.c

--- lab/lab7/lab7.c
+++ lab/lab7/lab7.c
@@ -14,34 +14,68 @@ const int NUM_ACTIVITIES = 5;
 const int MAX_LINE = 20;
 
 // Function Prototypes:
-void initArrays(float activity_level[], char activity_type[][MAX_LINE]);
+void initArrays(double levels[], char types[][MAX_LINE]);
 double getUserInput();
-void printCaloriesNeeded(double bmr, double activity_level[],
-	char activity_type[][MAX_LINE]);
+void printCaloriesNeeded(double basal_metabolic_rate, double levels[],
+	char types[][MAX_LINE]);
 
 int main(void) {
-	int i, j, gender, weight_lbs, height_in, age;
-	float weight_kg, height_cm;
-	double bmr, calories;
-	char line_in[MAX_LINE];
 	char activity_type[NUM_ACTIVITIES][MAX_LINE];
-	float activity_level[NUM_ACTIVITIES];
+	double activity_level[NUM_ACTIVITIES];
+	double bmr;
 
-	// Get values from redirection
+	// Initalize Arrays
+	initArrays(activity_level, activity_type);
+	
+	// COMMENT THIS LINE OUT BEFORE GRADESCOPE SUBMISSION:
+	freopen("/dev/tty", "rw", stdin);
+	
+	// Ask user for values
+	bmr = getUserInput();
+
+	// Print BMR results
+	printf("\n\nYour BMR (basal metabolic rate) is %.1lf", bmr);
+	printf("\n\nBMR represents the amount of calories you burn at rest."
+			 "\n\nTotal daily calories needed to maintain your current "
+			 "weight at a\nparticular activity levels is "
+			 "your BMR * that activity level value:\n\n");
+	
+	// Calculate and print daily calorie intake results
+	printCaloriesNeeded(bmr, activity_level, activity_type);
+
+}
+
+/* ------------------------------------------------------------ */
+/* initalizes arrays from stdin (redirection of file expected)
+	parameters: levels[], types[][] (The activity levels and types)
+	return: none
+*/
+void initArrays(double levels[], char types[][MAX_LINE]) {
+	int i, j;
+	char line_in[MAX_LINE];
 	for (i = 0; i/2 < NUM_ACTIVITIES; i++) {
 		if (i % 2 == 0) {
 			fgets(line_in, MAX_LINE, stdin);
 			for (j = 0; j < MAX_LINE; j++) {
-				activity_type[i/2][j] = line_in[j];
+				types[i/2][j] = line_in[j];
 			}
 		}
 		else if (i % 2 == 1) {
-			scanf("%f\n", &activity_level[i/2]);
+			scanf("%lf\n", &levels[i/2]);
 		}
 	}
-	// Ask user for values
-	// COMMENT THIS LINE OUT BEFORE GRADESCOPE SUBMISSION:
-	freopen("/dev/tty", "rw", stdin);
+}
+
+/* ------------------------------------------------------------ */
+/*	gets user input: boy or girl, weight, height, & age
+ 	and calculates the BMR value for that user
+ 	parameters: none
+ 	return: the BMR value
+*/
+double getUserInput() {
+	int gender, weight_lbs, height_in, age;
+	float weight_kg, height_cm;
+	double basal_metabolic_rate;
 
 	while (gender != 1 && gender != 2) {
 		printf("\n\nAre you a boy or a girl? 1 for boy, 2 for girl: ");
@@ -59,21 +93,26 @@ int main(void) {
 	height_cm = height_in * 2.54;
 
 	if (gender == 1)
-		bmr = (10 * weight_kg + 6.25 * height_cm - 5 * age + 5);
+		basal_metabolic_rate = (10 * weight_kg + 6.25 * height_cm - 5 * age + 5);
 	else if (gender == 2)
-		bmr = (10 * weight_kg + 6.25 * height_cm - 5 * age - 161);
-
-	// Print BMR results
-	printf("\n\nYour BMR (basal metabolic rate) is %.1lf", bmr);
-	printf("\n\nBMR represents the amount of calories you burn at rest."
-			 "\n\nTotal daily calories needed to maintain your current "
-			 "weight at a\nparticular activity levels is "
-			 "your BMR * that activity level value:\n\n");
+		basal_metabolic_rate = (10 * weight_kg + 6.25 * height_cm - 5 * age - 161);
 	
-	// Calculate and print daily calorie intake results
-	for (i = 0; i < NUM_ACTIVITIES; i++) {
-		calories = bmr * activity_level[i];
-		printf("%.1lf calories per day for %s", calories, activity_type[i]);
-	}
+	return basal_metabolic_rate;
+}
 
+/* ------------------------------------------------------------ */
+/*	prints the calories needed for each activity level.
+	parameters: bmr, levels[], & types[][] (the calculated bmr,
+	and the activity levels and types)
+	returns: none
+*/
+void printCaloriesNeeded(double basal_metabolic_rate, double levels[],
+   char types[][MAX_LINE]) {
+	int i;
+	double calories;
+
+	for (i = 0; i < NUM_ACTIVITIES; i++) {
+		calories = basal_metabolic_rate * levels[i];
+		printf("%.1lf calories per day for %s", calories, types[i]);
+	}
 }
