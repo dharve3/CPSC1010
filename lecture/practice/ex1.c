0aa124ff198bb86ef6ad5ece1ea5d59276bac099
DylanJoey17 <dharve3@clemson.edu>
2023-10-31 15:22:47 -0400

    Added comments to ex1.c

--- lecture/practice/ex1.c
+++ lecture/practice/ex1.c
@@ -8,9 +8,11 @@ typedef struct {
 
 int main(int argc, char *argv[]) {
 	dob bday;
+	// dob bday = {"Jan", 31, 1967}; 
 
 	printf("\nEnter your birthday with 3 letter month day year:  ");
 	scanf("%s %d %d", bday.month, &bday.day, &bday.year);
+	// No '&' for array since it already refs address
 	
 	printf("\nYour birthday is: %s %d, %d\n\n", bday.month, bday.day, bday.year);
 
